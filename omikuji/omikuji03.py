fcd105573ef9b402a05d3ac6540a59e880f7e810
Ayumu Takai <atakai@hackwork.jp>
2023-10-30 16:29:09 +0900

    作業ミスでファイルが上書きされていた不具合修正

--- omikuji/omikuji03.py
+++ omikuji/omikuji03.py
@@ -6,16 +6,8 @@
 import random
 
 # 以下の確率は合計が100になるようにしてください
-omikuji = [
-    {"name": "大吉", "rate": 20, "field": {
-        "願望": "多く望まなければかなう", "恋愛": "良い人は近くにいる", "学問": "努力は実る"}},
-    {"name": "中吉", "rate": 40, "field": {
-        "願望": "叶うまでまて", "恋愛": "迷うべからず", "学問": "疲れたときには休め"}},
-    {"name": "小吉", "rate": 30, "field": {
-        "願望": "かならずかなう", "恋愛": "目移りは避けよ", "学問": "伸びる時期"}},
-    {"name": "凶", "rate": 10, "field": {
-        "願望": "立ち止まって見直せ", "恋愛": "あせるべからず", "学問": "集中して臨め"}}
-]
+omikuji = [["大吉", 20], ["中吉", 40], ["小吉", 30], ["凶", 10]]
+
 
 # 1〜100の乱数を得る
 index = random.randint(1, 100)
@@ -25,10 +17,7 @@ rate_sum = 0
 
 # 確率の累計が乱数以上になるまで繰り返す
 for result in omikuji:
-    rate_sum += result["rate"]
+    rate_sum += result[1]
     if index <= rate_sum:
-        print("==", result["name"], "==")
-        print("願望", result["field"]["願望"])
-        print("恋愛", result["field"]["恋愛"])
-        print("学問", result["field"]["学問"])
+        print(result[0])
         break
